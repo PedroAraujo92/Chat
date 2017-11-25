a7971dc601170f32830fec7ece3219503617fa3b
PedroAraujo92 <pe.araujo21@gmail.com>
2017-11-25 09:36:19 -0200

    Exibição dos usuários conectados

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,7 @@
+var name = "";
 function startChat(){
-	var name = document.getElementById('txtNome').value;
+	name = document.getElementById('txtNome').value;
+
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		// 4 DONE | 200 OK
@@ -10,7 +12,9 @@ function startChat(){
 			// name = document.getElementById('txtNome');
 			document.getElementById('container').innerHTML = conteudo_chat;
 			document.getElementById('chat_user').innerHTML = name;
-			document.getElementById('users').innerHTML = "Usuários conectados: " + name;
+			getConnectedUsers();
+			// var all_users = getConnectedUsers();
+			// document.getElementById('users').innerHTML = "Usuários conectados: " + getConnectedUsers();//name;
 		}
 
 	}
@@ -18,3 +22,40 @@ function startChat(){
 	xhttp.open("GET", "chat.html", true);
 	xhttp.send();
 }
+
+function getConnectedUsers(){
+	var online_users = "";
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		// 4 DONE | 200 OK
+		if (this.readyState == 4 && this.status == 200) {
+			var json = JSON.parse(this.responseText);
+
+			for(var i = 0; i < json.length; i++) {
+				online_users += json[i] + ' | ';
+			}
+
+			online_users += ' | ' + name;
+
+			document.getElementById('users').innerHTML = "Usuários conectados: " + online_users;
+			console.log(online_users);
+		}
+	}
+
+	xhttp.open("GET", "http://www.angelito.com.br/webchat/users", true);
+	xhttp.send();
+}
+
+/*function isConnectedUser(user_name){
+	var xhttp = new XMLHttpRequest();
+	// var user_name = "";
+	xhttp.onreadystatechange = function(){
+		// 4 DONE | 200 OK
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+			 console.log(this.responseText);
+		}
+	}
+
+	xhttp.open("POST", "http://www.angelito.com.br/webchat/user?nickname=" + user_name);
+	xhttp.send();
+}*/
