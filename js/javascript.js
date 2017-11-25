9b6091ae20d1093426a2a675c2d5ac7c0dfccbd6
PedroAraujo92 <pe.araujo21@gmail.com>
2017-11-25 10:31:54 -0200

    Exibicao de mensagens de chat sem formatacao

--- js/javascript.js
+++ js/javascript.js
@@ -11,9 +11,9 @@ function startChat(){
 			// window.location.href="chat.html";
 			// name = document.getElementById('txtNome');
 			document.getElementById('container').innerHTML = conteudo_chat;
-			document.getElementById('chat_user').innerHTML = name;
-			getConnectedUsers();
-			// var all_users = getConnectedUsers();
+			// document.getElementById('chat_user').innerHTML = name;
+			isConnectedUser();
+			// var all_users = getConnected	Users();
 			// document.getElementById('users').innerHTML = "Usuários conectados: " + getConnectedUsers();//name;
 		}
 
@@ -35,10 +35,8 @@ function getConnectedUsers(){
 				online_users += json[i] + ' | ';
 			}
 
-			online_users += ' | ' + name;
-
 			document.getElementById('users').innerHTML = "Usuários conectados: " + online_users;
-			console.log(online_users);
+			// console.log(online_users);
 		}
 	}
 
@@ -46,16 +44,48 @@ function getConnectedUsers(){
 	xhttp.send();
 }
 
-/*function isConnectedUser(user_name){
+function loadWebChat(){
+	var messages = "";
 	var xhttp = new XMLHttpRequest();
-	// var user_name = "";
 	xhttp.onreadystatechange = function(){
 		// 4 DONE | 200 OK
-		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-			 console.log(this.responseText);
+		if (this.readyState == 4 && this.status == 200) {
+			var json = JSON.parse(this.responseText);
+
+			for(var i = 0; i < json.length; i++) {
+				var item = json[i];
+				if (item.user == name) {
+          			messages += '<div class="content right"><span class="user"><span class="date">['+item.datetime+']</span> Você diz:</span><span class="message">'+item.textmsg+'</span></div>';
+        		} else {
+          			messages += '<div class="content left"><span class="user"><span class="date">['+item.datetime+']</span> '+item.user+' diz:</span><span class="message">'+item.textmsg+'</span></div>';
+        		}
+			}
+
+			document.getElementById("message").innerHTML = messages;
+			// console.log(this.responseText + " " + "http://www.angelito.com.br/webchat/messages?nickname=" + name);
 		}
 	}
 
-	xhttp.open("POST", "http://www.angelito.com.br/webchat/user?nickname=" + user_name);
+	xhttp.open("GET", "http://www.angelito.com.br/webchat/messages?nickname=" + name, true);
+  	xhttp.send();
+}
+
+function isConnectedUser(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		// 4 DONE | 200 OK
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+			if (this.responseText == "OK") {
+       			getConnectedUsers();
+				loadWebChat();
+     		} else {
+      	 		alert(this.responseText);
+     		}
+
+			 document.getElementById('chat_user').innerHTML = name;
+		}
+	}
+
+	xhttp.open("POST", "http://www.angelito.com.br/webchat/user?nickname=" + name);
 	xhttp.send();
-}*/
+}
